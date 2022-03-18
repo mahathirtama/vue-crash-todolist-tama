d06b86ce36d163e3d9c6702d17097ec45ea07f74
mahathirtama <mahathirtama.ahmad@gmail.com>
2022-03-19 03:20:38 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div id="app">
+  <Header />
+  <router-view/>
+</div>
+  <!-- <nav> -->
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    
+  <!-- </nav> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/layout/Header.vue';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,6 +28,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
