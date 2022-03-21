449a746c8622aa13d9e700e1de344e0f7149089c
mahathirtama <mahathirtama.ahmad@gmail.com>
2022-03-21 15:37:27 +0700

    benerin todo second commit

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-bind:key="todo.id" v-for="todo in todos">
-            <TodoItems v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+            <TodoItems v-bind:todo="todo"  v-on:del-todo="$emit('del-todo', todo.id)" />
         </div>
     </div>
 </template>
@@ -10,7 +10,7 @@
 import TodoItems from "./TodoItems.vue";
 
 export default {
-    name: 'Todos',
+    name: 'TodosVue',
     components: {
         TodoItems
     },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,51 @@
 <template>
-<div id="app">
-  <AddTodo v-on:add-todo="addTodo" />
-  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
-</div>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
-import Todos from '../components/Todos.vue';
-import AddTodo from '../components/AddTodo.vue';
-import axios from 'axios';
+import Todos from "../components/Todos.vue";
+import AddTodo from "../components/AddTodo.vue";
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     Todos,
-    AddTodo
-},
+    AddTodo,
+  },
   data() {
     return {
-      todos: []
-    }
+      todos: [],
+    };
   },
   methods: {
     deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
-      .catch(err => console.log(err))
-      
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then((this.todos = this.todos.filter((todo) => todo.id !== id)))
+        .catch((err) => console.log(err));
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
-      axios.post('https://jsonplaceholder.typicode.com/todos',{
-        title,
-        completed
-      })
-      .then(res => this.todos = [...this.todos, res.data])
-      .catch(err => console.log(err))
-    }
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
   },
   created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-    .then(res => this.todos = res.data)
-    .catch(err => console.log(err));
-  }
-}
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 
 <style>
